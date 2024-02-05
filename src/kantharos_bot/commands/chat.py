35c25597315bcf5386c91635861537cdf88c9d9a
Andre Faria <andremarcalfaria@gmail.com>
2024-02-05 22:47:48 +0000

    context working

--- src/kantharos_bot/commands/chat.py
+++ src/kantharos_bot/commands/chat.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import pathlib
 from typing import Dict, List
 
@@ -31,27 +32,28 @@ async def chat(ctx: Context, *prompt: str):
     prompt_string = " ".join(prompt)
     temp_file = pathlib.Path(settings.openchat.temp_file_path)
 
-    if temp_file.exists():
-        if prompt_string == settings.openchat.clean_history_command:
-            logging.info("History clean command given")
-            temp_file.unlink(missing_ok=True)  # Deletes temp file if clean history command is given
-            clean = True
-        else:
-            logging.info("File exists")
-            with open(temp_file.resolve(), mode="r") as fp:
-                assistant_content = fp.readlines()
-
-            assistant_content.insert(0, settings.openchat.default_assistant_content)
+    if prompt_string == settings.openchat.clean_history_command:
+        logging.info("History clean command given")
+        temp_file.unlink(missing_ok=True)  # Deletes temp file if clean history command is given
+        clean = True
     else:
-        logging.info(f"History file not found. Creating history file {temp_file.resolve()}")
-        open(temp_file.resolve(), mode="a").close()  # Creates temp file if it does not exist
-        assistant_content = [settings.openchat.default_assistant_content]
+        if temp_file.exists():
+            logging.info("History file exists. Reading it for context.")
+            with open(temp_file.resolve(), mode="r") as fp:
+                assistant_content = fp.read()
+        else:
+            logging.info(f"History file not found. Creating history file {temp_file.resolve()}.")
+            open(temp_file.resolve(), mode="a").close()  # Creates temp file if it does not exist
+            assistant_content = f"{settings.openchat.default_assistant_content} \n\n"
+
+        with open(temp_file.resolve(), mode="a") as fp:
+            fp.write(f"{prompt_string} \n\n")
 
     if not clean:
         prompt_list: List[Dict[str, str]] = [
             {"role": "user", "content": prompt_string},
             {"role": "system", "content": settings.openchat.default_system_content},
-            {"role": "assistant", "content": "\n".join(assistant_content)},
+            {"role": "assistant", "content": assistant_content},
         ]
 
         async with ctx.typing():
@@ -62,11 +64,28 @@ async def chat(ctx: Context, *prompt: str):
 
         content: str = completion.choices[0].message.content
 
-        with open(temp_file.resolve(), mode="w") as fp:
-            fp.write(f"{content}")
+        with open(temp_file.resolve(), mode="a") as fp:
+            fp.write(f"{content} \n\n")
 
         logging.info(completion.__str__())
 
         await ctx.send(content)
     else:
         await ctx.send("Chat history cleaned")
+
+    if (
+        temp_file.exists()
+        and os.path.getsize(settings.openchat.temp_file_path) > settings.openchat.history_max_size
+    ):
+        logging.info(
+            f"History file reached max size of {settings.openchat.history_max_size} bytes. Deleting."
+        )
+        temp_file.unlink(missing_ok=True)  # Deletes temp file if it reaches max size
+        await ctx.send(
+            "\n".join(
+                [
+                    f"History file reached max size of {settings.openchat.history_max_size} bytes.",
+                    "It has been deleted. Please restart the prompt chain.",
+                ]
+            )
+        )
